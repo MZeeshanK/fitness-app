6387270d789196817773ba86ff5e3bd56278d37c
MZeeshanK <malikzeeshankhurshid@gmail.com>
2024-02-08 00:52:11 +0530

    Created Navbar and Hamburger Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
+import { useEffect, useState } from "react";
+import Navbar from "./scenes/Navbar";
+import { SelectedPage } from "./shared/types";
+
 export default function App() {
-  return <div className="app bg-gray-20"></div>;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home,
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState(false);
+
+  useEffect(() => {
+    // Handling Navbar background change after scroll
+    function handleScroll() {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else setIsTopOfPage(false);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  );
 }
